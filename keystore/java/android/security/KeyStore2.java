0f32fb050e68afa233741503e4dd8f27590e3393
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    Initial implementation of KeyboxImitationHooks
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- keystore/java/android/security/KeyStore2.java
+++ keystore/java/android/security/KeyStore2.java
@@ -31,6 +31,8 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
+import com.android.internal.util.KeyboxImitationHooks;
+
 import java.util.Calendar;
 
 /**
@@ -249,7 +251,9 @@ public class KeyStore2 {
      */
     public KeyEntryResponse getKeyEntry(@NonNull KeyDescriptor descriptor)
             throws KeyStoreException {
-        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+
+        return KeyboxImitationHooks.onGetKeyEntry(
+                handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor)));
     }
 
     /**
