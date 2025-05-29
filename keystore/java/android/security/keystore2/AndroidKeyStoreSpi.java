bdd432350e84c3a2955640c718262e47ab048a6d
306bobby-android <143777624+306bobby-android@users.noreply.github.com>
2025-05-28 22:27:20 -0400

    PixelPropsUtils: re-enable hardware attestation
    Remove the keystore exception blocking hardware attestation used for safetynet (dead) and legacy Play Integrity verdicts (dead A13+)
    
    New A13+ verdicts require a hardware attest key for Basic Integrity, however the bootloader can be unlocked. By allowing hardware attestation and continuing to spoof beta props, devices with functioning TEE can pass basic and retain RCS functionality, as well as basic integrity for any apps requiring it (ChatGPT for example)
    
    Tested on Pixel 5a (barbet), Pixel 5 (redfin) and Pixel Tablet (tangorpro)(unofficial)

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -166,8 +166,6 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        PixelPropsUtils.onEngineGetCertificateChain();
-
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {
