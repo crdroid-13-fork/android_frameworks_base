2c1f2e015942b22490245b8aba766fdface0d1a9
Pranav Vashi <neobuddy89@gmail.com>
2025-06-08 17:19:44 +0530

    PixelPropsUtils: Remove gms spoof completely
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -123,7 +123,6 @@ import com.android.server.compat.PlatformCompatNative;
 import com.android.server.connectivity.PacProxyService;
 import com.android.server.contentcapture.ContentCaptureManagerInternal;
 import com.android.server.coverage.CoverageService;
-import com.android.server.crdroid.AttestationService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.DisplayManagerService;
@@ -2558,11 +2557,6 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(LinearmotorVibratorService.class);
                 t.traceEnd();
             }
-
-            // AttestationService
-            t.traceBegin("AttestationService");
-            mSystemServiceManager.startService(AttestationService.class);
-            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
