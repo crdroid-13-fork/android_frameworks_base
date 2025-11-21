5d1b5576057910d40ae3de1cf78c517287f117b7
Joey <joey@evolution-x.org>
2025-11-21 21:08:26 +0900

    PixelPropsUtils: Update fingerprints to March 2026 release
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- services/appprediction/java/com/android/server/appprediction/AppPredictionManagerService.java
+++ services/appprediction/java/com/android/server/appprediction/AppPredictionManagerService.java
@@ -179,7 +179,7 @@ public class AppPredictionManagerService extends
 
             Context ctx = getContext();
             if (!(ctx.checkCallingPermission(PACKAGE_USAGE_STATS) == PERMISSION_GRANTED
-                    || com.android.internal.util.matrixx.PixelPropsUtils.isSystemLauncher(Binder.getCallingUid())
+                    || com.android.internal.util.crdroid.PixelPropsUtils.isSystemLauncher(Binder.getCallingUid())
                     || mServiceNameResolver.isTemporary(userId)
                     || mActivityTaskManagerInternal.isCallerRecents(Binder.getCallingUid())
                     || Binder.getCallingUid() == Process.SYSTEM_UID)) {
