4ad6ad43131ac681e15dae6aad81aa7c6e5fb678
muralivijay <muralidharan9845@gmail.com>
2026-05-03 20:56:06 +0530

    spoof: Remove pif and gamespoof from spoofmanager
    * We already have legacy pif.
    * also We never use any kind of gamespoof.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- services/core/java/com/android/server/spoof/AxSpoofManager.java
+++ services/core/java/com/android/server/spoof/AxSpoofManager.java
@@ -35,8 +35,6 @@ public class AxSpoofManager implements IAxSpoofManager {
     private static final String TAG = "AxSpoofManager";
 
     private static final String[] WATCHED_KEYS = {
-            Settings.Secure.SPOOF_PIF_CONFIG,
-            Settings.Secure.SPOOF_GAMEPROPS_CONFIG,
             Settings.Secure.SPOOF_TRICKYSTORE_TARGET,
             Settings.Secure.SPOOF_TRICKYSTORE_KEYBOX,
             Settings.Secure.SPOOF_TRICKYSTORE_PATCH,
@@ -102,16 +100,6 @@ public class AxSpoofManager implements IAxSpoofManager {
 
     private String getCached(String key) {
         return mCache.get(key);
-    }
-
-    @Override
-    public String getPifConfig() {
-        return getCached(Settings.Secure.SPOOF_PIF_CONFIG);
-    }
-
-    @Override
-    public String getGamePropsConfig() {
-        return getCached(Settings.Secure.SPOOF_GAMEPROPS_CONFIG);
     }
 
     @Override
