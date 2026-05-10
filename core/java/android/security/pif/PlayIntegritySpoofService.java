27e3b982aac42f9c14caf9061450190117702fc4
muralivijay <muralidharan9845@gmail.com>
2026-05-10 12:54:46 +0530

    pif: Remove broken gphotos spoof.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- core/java/android/security/pif/PlayIntegritySpoofService.java
+++ core/java/android/security/pif/PlayIntegritySpoofService.java
@@ -31,26 +31,6 @@ public final class PlayIntegritySpoofService {
     private static final String DROIDGUARD_PACKAGE = "com.google.android.gms.unstable";
     private static final String VENDING_PACKAGE = "com.android.vending";
     private static final String GMS_PACKAGE = "com.google.android.gms";
-    private static final String GPHOTOS_PACKAGE = "com.google.android.apps.photos";
-
-    private static final Map<String, Object> PIXEL_XL_PROPS = Map.of(
-        "BRAND", "google",
-        "MANUFACTURER", "Google",
-        "DEVICE", "marlin",
-        "PRODUCT", "marlin",
-        "HARDWARE", "marlin",
-        "ID", "QP1A.191005.007.A3",
-        "MODEL", "Pixel XL",
-        "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
-    );
-
-    private static final Set<String> NEXUS_FEATURES = Set.of(
-        "com.google.android.apps.photos.NEXUS_PRELOAD",
-        "com.google.android.apps.photos.nexus_preload",
-        "com.google.android.feature.PIXEL_EXPERIENCE",
-        "com.google.android.feature.GOOGLE_BUILD",
-        "com.google.android.feature.GOOGLE_EXPERIENCE"
-    );
 
     private static final Set<String> PIXEL_FEATURES = Set.of(
         "com.google.android.feature.PIXEL_2022_EXPERIENCE",
@@ -113,7 +93,6 @@ public final class PlayIntegritySpoofService {
     private volatile boolean mSpoofSignature = false;
     private volatile boolean mSpoofVendingBuild = true;
     private volatile boolean mSpoofVendingSdk = false;
-    private volatile boolean mSpoofPhotos = false;
     private volatile boolean mDebug = false;
 
     private final Map<String, String> mBuildFields = new ConcurrentHashMap<>();
@@ -251,9 +230,6 @@ public final class PlayIntegritySpoofService {
                 break;
             case "spoofVendingSdk":
                 mSpoofVendingSdk = "1".equals(value) || "true".equalsIgnoreCase(value);
-                break;
-            case "spoofPhotos":
-                mSpoofPhotos = "1".equals(value) || "true".equalsIgnoreCase(value);
                 break;
             case "DEBUG":
                 mDebug = "1".equals(value) || "true".equalsIgnoreCase(value);
@@ -505,23 +481,8 @@ public final class PlayIntegritySpoofService {
         return Base64.decode(ROM_SIGNATURE_DATA, Base64.DEFAULT);
     }
 
-    public boolean shouldSpoofPhotos(String packageName) {
-        return mConfigLoaded && mSpoofPhotos && TextUtils.equals(GPHOTOS_PACKAGE, packageName);
-    }
-
-    public void spoofPhotosProps() {
-        for (Map.Entry<String, Object> entry : PIXEL_XL_PROPS.entrySet()) {
-            spoofField(entry.getKey(), String.valueOf(entry.getValue()), "Photos");
-        }
-        Log.i(TAG, "Photos spoofing enabled - device appears as Pixel XL");
-    }
-
     public Boolean hasSystemFeature(String name, int version) {
         final String pkgName = ActivityThread.currentPackageName();
-        if (shouldSpoofPhotos(pkgName)) {
-            if (!isPixelDevice() && PIXEL_FEATURES.contains(name)) return false;
-            return NEXUS_FEATURES.contains(name);
-        }
         return null;
     }
 
